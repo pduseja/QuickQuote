dbe3578eda135fec94f646e368b3319b155aa7e2
pduseja <dusejapt@gmail.com>
2017-04-07 18:37:15 +0530

    Add files via upload

--- src/main/java/com/tf/controller/QuickQuoteController.java
+++ src/main/java/com/tf/controller/QuickQuoteController.java
@@ -1,6 +1,7 @@
 package com.tf.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +17,7 @@ public class QuickQuoteController {
 	@Autowired
     private QuickQuotes quickQuotes;
     
+	@CrossOrigin(origins="*")
     @RequestMapping(value="/QQ",method=RequestMethod.POST)
     public QuoteResponse getQuote(@RequestBody Quote quote) {
         QuoteResponse qe = quickQuotes.getQuote(quote);
