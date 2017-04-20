579e25489806b22ef2cafde7661f52b9c2f5134f
pduseja <dusejapt@gmail.com>
2017-04-20 13:04:27 +0530

    Add files via upload

--- src/main/java/com/tf/QuickQuoteApplication.java
+++ src/main/java/com/tf/QuickQuoteApplication.java
@@ -4,7 +4,9 @@ import java.sql.SQLException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class QuickQuoteApplication {
 
